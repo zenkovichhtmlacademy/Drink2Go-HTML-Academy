695f8f5e2e4fb50781bfde7d992b56f046557af6
Александр Белостоцкий <belostozky1994@mail.ru>
2023-02-10 19:39:05 +0600

    Add nojs mobile header style

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,21 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.header__nav');
+const navToggle = document.querySelector('.header__nav-toggle');
+const body = document.querySelector('.page__body');
+const headerContainer = document.querySelector('header__container');
+
+// navMain.classList.remove('header__nav--nojs');
+headerContainer.classList.remove('header__container--nojs')
+navMain.classList.add('header__nav--closed');
+navMain.classList.remove('header__nav--opened');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header__nav--closed')) {
+    navMain.classList.remove('header__nav--closed');
+    navMain.classList.add('header__nav--opened');
+    body.classList.add('page__body--locked');
+  } else {
+    navMain.classList.add('header__nav--closed');
+    navMain.classList.remove('header__nav--opened');
+    body.classList.remove('page__body--locked');
+  }
+});
