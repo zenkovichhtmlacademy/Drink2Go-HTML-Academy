8cf43aa95670f359c6e15e3d2106936d85c8eacc
Александр Белостоцкий <belostozky1994@mail.ru>
2023-02-15 17:01:13 +0600

    Fix catalog alignment

--- source/js/script.js
+++ source/js/script.js
@@ -62,29 +62,3 @@ function onSelectListClick (evt) {
     selectToggle.classList.remove('custom-select__toggle--checked');
   }
 }
-
-// Swiper:
-
-Swiper.use([Navigation, Pagination]);
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
